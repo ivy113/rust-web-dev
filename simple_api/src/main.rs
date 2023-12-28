1540a5be4a21f7a0111ed3bfeb7b7f2855bb904a
Ivy Huang <ivyhuang@Ivys-iMac.the-beach.net>
2023-12-28 16:24:03 -0500

    upload addl files

---
+++ simple_api/src/main.rs
@@ -0,0 +1,119 @@
+use warp::{Filter, reject::Reject, Rejection, Reply, http::StatusCode, http::Method};
+// use std::fmt;
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+struct Question {
+    id:QuestionId,
+    title:String,
+    content:String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug, Serialize)]
+struct QuestionId(String);
+
+#[derive(Debug)]
+struct InvalidId;
+
+impl Question {
+    fn new(
+        id:QuestionId,
+        title:String,
+        content:String,
+        tags:Option<Vec<String>>
+    ) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err (
+                Error::new(ErrorKind::InvalidInput, "No id provided")
+            )
+        }
+    }
+}
+
+impl Reject for InvalidId {}
+
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!("faq".to_string()))
+    );
+
+    match question.id.0.parse::<i32>() {
+        Err(_) => {
+            Err(warp::reject::custom(InvalidId))
+        },
+        Ok(_) => {
+            Ok(warp::reply::json(
+                &question
+            ))
+        }
+    }
+}
+
+async fn return_error(r:Rejection) -> Result<impl Reply, Rejection>{
+    if let Some(_invalid_id) = r.find::<InvalidId>() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented",
+            StatusCode::UNPROCESSABLE_ENTITY
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found",
+            StatusCode::NOT_FOUND
+        ))
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        .allow_methods(
+            &[Method::PUT, Method::DELETE, Method::GET, Method:: POST]
+        );
+
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions)
+        .recover(return_error);
+
+    let routes = get_items.with(cors);
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
+    // let hello = warp::get()
+    //     .map(|| format!("Hello, iv!"));
+//
+//     let hi = warp::path("hello")
+//         .and(warp::path::param())
+//         .and(warp::header("user-agent"))
+//         .map(|param: String, agent: String|
+//             format!("Hello {}, whose agent is {}", param, agent)
+//         );
+//     warp::serve(hi)
+//         .run(([127,0,0,1],3030))
+//         .await;
+// }
